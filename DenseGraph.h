3db16fd9d6789adcd5f7def5014941cfa523c109
Ashwin Krishnamurthy <krishn_a1@denison.edu>
2024-04-07 17:26:51 -0400

    minor fix

--- DenseGraph.h
+++ DenseGraph.h
@@ -20,7 +20,7 @@ public:
 			DenseGraph	(int V, int E);
  			~DenseGraph	(void) override;
 			DenseGraph	(const DenseGraph& graph1) override;
-	DenseGraph<T>	operator=	(const DenseGraph<T> &graph1) override;
+	DenseGraph	operator=	(const DenseGraph &graph1) override;
 	bool		isEdge		(int v1, int v2) override;
 	int		getWeight	(int v1, int v2) override;
 	void		insertEdge	(int v1, int v2, int w) override;
